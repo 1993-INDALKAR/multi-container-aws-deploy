23b2febe3e70ce8b0b3654da37cc71b20ac87f7c
1993-INDALKAR <harishindalkar@yahoo.com>
2020-05-06 16:42:15 -0400

    aws change deploy

--- server/index.js
+++ server/index.js
@@ -23,11 +23,13 @@ const { Pool,Client } = require('pg');
 
 const pgClient = new Pool({
     user: keys.pgUser,
-    host: 'postgres',
+    // user: 'postgres',
+    host: keys.pgHost,
     database: keys.pgDatabase,
+    // database: 'postgres',
     // password: encodeURIComponent(keys.pgPassword),
-    password: 'postgres_password',
-    port: keys.pgPort
+    password: keys.pgPassword,
+    port: 6379
 });
 
 
@@ -41,38 +43,41 @@ const pgClient = new Pool({
 //     port: keys.pgPort
 // });
 
-const pgPoolWrapper = {
-    async connect() {
-        for (let nRetry = 1; ; nRetry++) {
-            try {
-                const client = await pgClient.connect();
-                if (nRetry > 1) {
-                    console.info('Now successfully connected to Postgres');
-                }
-                return client;
-            } catch (e) {
-                if (e.toString().includes('ECONNREFUSED') && nRetry < 5) {
-                    console.info('ECONNREFUSED connecting to Postgres, ' +
-                        'maybe container is not ready yet, will retry ' + nRetry);
-                    // Wait 1 second
-                    await new Promise(resolve => setTimeout(resolve, 1000));
-                } else {
-                    throw e;
-                }
-            }
-        }
-    }
-};
+// const pgPoolWrapper = {
+//     async connect() {
+//         for (let nRetry = 1; ; nRetry++) {
+//             try {
+//                 const client = await pgClient.connect();
+//                 if (nRetry > 1) {
+//                     console.info('Now successfully connected to Postgres');
+//                 }
+//                 return client;
+//             } catch (e) {
+//                 if (e.toString().includes('ECONNREFUSED') && nRetry < 5) {
+//                     console.info('ECONNREFUSED connecting to Postgres, ' +
+//                         'maybe container is not ready yet, will retry ' + nRetry);
+//                     // Wait 1 second
+//                     await new Promise(resolve => setTimeout(resolve, 1000));
+//                 } else {
+//                     throw e;
+//                 }
+//             }
+//         }
+//     }
+// };
 
 
 
 pgClient.on('error',()=>console.log('Lost PG connection'));
 
-pgClient.query("CREATE TABLE IF NOT EXISTS values (number INT)")
+
+
+ pgClient.query("CREATE TABLE IF NOT EXISTS values (number INT)")
     .catch(error=>{
         console.log(error);
         console.log("Error in creating Table in postgres");
     });
+
 
 
 const redis  = require('redis');
@@ -89,7 +94,12 @@ app.get('/',(req,res)=>{
 
 try{
     app.get('/values/all',async(req,res)=>{
-        const values = await pgClient.query('SELECT * from values');
+        // console.log(pgClient);
+        const values = await pgClient.query('SELECT * from values')
+                                     .catch(e=>{
+                                         console.log(e);
+                                         console.log("error in select query");
+                                     });
         console.log("value"+values.rows);
         res.send(values.rows);
     });
