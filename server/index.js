8cf20e7cbf6c8637e4e49ca730b26f89d59787fa
1993-INDALKAR <harishindalkar@yahoo.com>
2020-05-06 17:26:10 -0400

    changes server file

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
-const express = require("express");
+const keys = require('./keys');
 
-
-
-
-const keys = require("./keys");
+// Express App Setup
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -11,151 +9,62 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
-const { Pool,Client } = require('pg');
-// const pgClient = new Client(`postgres://${keys.pgUser}:${keys.pgPassword}@{keys.pgDatabase}:5432/${keys.pgDatabase}`);
-// pgClient.connect(function(err){
-//     if(err){
-//         console.log("error in connect")
-//         console.log(err);
-//     }
-// })
-
-// const pgClient = new Pool({
-//     user: keys.pgUser,
-//     // user: 'postgres',
-//     host: keys.pgHost,
-//     database: keys.pgDatabase,
-//     // database: 'postgres',
-//     // password: encodeURIComponent(keys.pgPassword),
-//     password: keys.pgPassword,
-//     port: 6379
-// });
-
-const connectingString = `postgres://${keys.pgUser}:${keys.pgPassword}@${keys.host}/${keys.pgDatabase}`;
-
-const pgClient = new Client({
-    connectionString: connectingString
+// Postgres Client Setup
+const { Pool } = require('pg');
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
 });
+pgClient.on('error', () => console.log('Lost PG connection'));
 
-// const pgClient = new Pool({
-//     user: keys.pgUser,
-//     host: keys.pgHost,
-//     database: keys.pgDatabase,
-//     password: keys.pgPassword,
-//     port: keys.pgPort
-// });
+pgClient
+  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+  .catch(err => console.log(err));
 
-// const pgPoolWrapper = {
-//     async connect() {
-//         for (let nRetry = 1; ; nRetry++) {
-//             try {
-//                 const client = await pgClient.connect();
-//                 if (nRetry > 1) {
-//                     console.info('Now successfully connected to Postgres');
-//                 }
-//                 return client;
-//             } catch (e) {
-//                 if (e.toString().includes('ECONNREFUSED') && nRetry < 5) {
-//                     console.info('ECONNREFUSED connecting to Postgres, ' +
-//                         'maybe container is not ready yet, will retry ' + nRetry);
-//                     // Wait 1 second
-//                     await new Promise(resolve => setTimeout(resolve, 1000));
-//                 } else {
-//                     throw e;
-//                 }
-//             }
-//         }
-//     }
-// };
-
-
-
-pgClient.on('error',()=>console.log('Lost PG connection'));
-
-
-
- pgClient.query("CREATE TABLE IF NOT EXISTS values (number INT)")
-    .catch(error=>{
-        console.log(error);
-        console.log("Error in creating Table in postgres");
-    });
-
-
-
-const redis  = require('redis');
+// Redis Client Setup
+const redis = require('redis');
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
 });
-
 const redisPublisher = redisClient.duplicate();
-app.get('/',(req,res)=>{
-    res.send("Hi");
+
+// Express route handlers
+
+app.get('/', (req, res) => {
+  res.send('Hi');
 });
 
-try{
-    app.get('/values/all',async(req,res)=>{
-        // console.log(pgClient);
-        const values = await pgClient.query('SELECT * from values')
-                                     .catch(e=>{
-                                         console.log(e);
-                                         console.log("error in select query");
-                                     });
-        console.log("value"+values.rows);
-        res.send(values.rows);
-    });
-}
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values');
 
-catch(error){
-    console.log("Error occured here in all");
-    console.log(error);
-};
-
-
-try{
-    app.get('/values/current',async(req,res)=>{
-        redisClient.hgetall('values',(err,values)=>{
-            // console.log("Current values"+ JSON.parse(values));
-            res.send(values);
-        });
-    });
-}
-catch(error){
-    console.log("Error occured here in current");
-    console.log(error);
-}
-
-
-app.post('/values',async(req,res)=>{
-    
-    const index = req.body.index;
-    console.log(index);
-
-    if(parseInt(index)  > 40){
-        return res.status(422).send('Index too high');
-    }
-
-    redisClient.hset('values',index,'Nothing yet!');
-    redisPublisher.publish('insert',index);
-
-
-    redisClient.hgetall('values',(err,values)=>{
-        console.log("Current values"+ values);
-    });
-
-
-    pgClient.query('INSERT INTO values(number) VALUES($1)',[index]);
-
-    res.send({working: true});
-
+  res.send(values.rows);
 });
 
-app.listen(5000,err => {
-    console.log('Listening at 5000');
-})
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
+});
 
+app.post('/values', async (req, res) => {
+  const index = req.body.index;
 
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high');
+  }
 
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
+  res.send({ working: true });
+});
+
+app.listen(5000, err => {
+  console.log('Listening');
+});
